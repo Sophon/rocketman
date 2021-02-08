4a349e0a49fe4e82026c2fae1481c3b6b9953696
Le Duy Anh <lenduya@gmail.com>
2021-02-08 18:15:10 +0100

    Refactor variables

--- app/src/main/java/com/example/rocketman/MainActivity.kt
+++ app/src/main/java/com/example/rocketman/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.rocketman.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbarMain)
 
-        setupNavigation()
+        setupDrawerLayout()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
@@ -37,37 +40,26 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setupNavigation() {
-        toggle = ActionBarDrawerToggle(this, binding.drawerMain, R.string.open, R.string.close)
-            .also { toggle ->
-                binding.drawerMain.addDrawerListener(toggle)
-                toggle.syncState() //TODO: find out what it does
-            }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
+    }
 
-        binding.drawerContentMain.setNavigationItemSelectedListener { menuItem ->
-            when(menuItem.itemId) {
-                R.id.company_data -> {
-                    displayToast("Company data")
-                }
-
-                R.id.rockets_data -> {
-                    displayToast("Rocket data")
-                }
-
-                R.id.past_launches -> {
-                    displayToast("Past launches")
-                }
-
-                R.id.upcoming_launches -> {
-                    displayToast("Upcoming launches")
-                }
-            }
-
-            true
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 
-    private fun displayToast(string: String) {
-        Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
+    private fun setupDrawerLayout() {
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+            .also { toggle ->
+                binding.drawerLayout.addDrawerListener(toggle)
+                toggle.syncState() //TODO: find out what it does
+            }
+
+        binding.navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
     }
 }
---
+++ app/src/main/java/com/example/rocketman/company/CompanyDataFragment.kt
@@ -0,0 +1,5 @@
+package com.example.rocketman.company
+
+import androidx.fragment.app.Fragment
+
+class CompanyDataFragment: Fragment() {}
