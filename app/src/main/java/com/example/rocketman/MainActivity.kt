42efbb5d70891d95aff448a8770d2a90843e5d51
Le Duy Anh <lenduya@gmail.com>
2021-02-08 17:55:04 +0100

    Create skeleton for Rockets data

--- app/src/main/java/com/example/rocketman/MainActivity.kt
+++ app/src/main/java/com/example/rocketman/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.Navigation
 import com.example.rocketman.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.fragment_nav_host)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                     displayToast("Company data")
                 }
 
-                R.id.rocket_data -> {
+                R.id.rockets_data -> {
                     displayToast("Rocket data")
                 }
 
---
+++ app/src/main/java/com/example/rocketman/rocket/RocketsDataFragment.kt
@@ -0,0 +1,5 @@
+package com.example.rocketman.rocket
+
+import androidx.fragment.app.Fragment
+
+class RocketsDataFragment: Fragment() {}
