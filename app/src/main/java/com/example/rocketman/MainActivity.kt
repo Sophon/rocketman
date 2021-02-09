c9654f728c4c443a0c8dd2b8d5c5f2555c94d9af
Le Duy Anh <lenduya@gmail.com>
2021-02-09 16:55:43 +0100

    Extract drawer from activity to fragment

--- app/src/main/java/com/example/rocketman/MainActivity.kt
+++ app/src/main/java/com/example/rocketman/MainActivity.kt
@@ -15,49 +15,46 @@ import com.example.rocketman.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private val navController by lazy {
-        (supportFragmentManager
-            .findFragmentById(R.id.fragment_nav_host) as NavHostFragment).navController
-    }
+//    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private val navController by lazy {
+//        (supportFragmentManager
+//            .findFragmentById(R.id.fragment_nav_host) as NavHostFragment).navController
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbarMain)
-        setupNavigationDrawer()
     }
 
-    private fun setupNavigationDrawer() {
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.homeFragment,
-            R.id.rocketsDataFragment,
-            R.id.companyDataFragment
-        ), binding.drawerLayout)
-
-        binding.navView.setupWithNavController(navController)
-        NavigationUI.setupActionBarWithNavController(
-            this,
-            navController,
-            binding.drawerLayout
-        )
-        NavigationUI.setupWithNavController(binding.navView, navController)
-    }
+//    private fun setupNavigationDrawer() {
+//        appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.homeFragment,
+//            R.id.rocketsDataFragment,
+//            R.id.companyDataFragment
+//        ), binding.drawerLayout)
+//
+//        binding.navView.setupWithNavController(navController)
+//        NavigationUI.setupActionBarWithNavController(
+//            this,
+//            navController,
+//            binding.drawerLayout
+//        )
+//        NavigationUI.setupWithNavController(binding.navView, navController)
+//    }
 
     //region Navigation
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    override fun onBackPressed() {
-        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
+//
+//    override fun onBackPressed() {
+//        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            binding.drawerLayout.closeDrawer(GravityCompat.START)
+//        } else {
+//            super.onBackPressed()
+//        }
+//    }
     //endregion
 }
