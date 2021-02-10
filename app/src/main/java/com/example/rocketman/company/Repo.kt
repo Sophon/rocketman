56054ce52ff0983763a48ba3c52e40221da6fbf5
Le Duy Anh <lenduya@gmail.com>
2021-02-10 23:09:39 +0100

    Implement repo

---
+++ app/src/main/java/com/example/rocketman/company/Repo.kt
@@ -0,0 +1,38 @@
+package com.example.rocketman.company
+
+import android.content.Context
+import com.example.rocketman.common.BASE_URL_SPACEX
+import com.example.rocketman.company.net.SpaceXApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.IllegalStateException
+
+private const val ERROR_MSG_NO_INSTANCE = "Company repository must be initialized!"
+
+class Repo private constructor(context: Context) {
+
+    private val api by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL_SPACEX)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(SpaceXApi::class.java)
+    }
+
+    suspend fun getCompanyData() = api.getCompanyInfo()
+
+    companion object {
+        private var INSTANCE: Repo? = null
+
+        fun init(context: Context) {
+            if(INSTANCE == null) {
+                INSTANCE = Repo(context)
+            }
+        }
+
+        fun get(): Repo {
+            return INSTANCE ?: throw IllegalStateException(ERROR_MSG_NO_INSTANCE)
+        }
+    }
+}
