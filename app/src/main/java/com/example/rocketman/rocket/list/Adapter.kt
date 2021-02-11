1b97a8cc5943d678c3560f4d9cd1e0f217c112ac
Le Duy Anh <lenduya@gmail.com>
2021-02-11 23:47:29 +0100

    Implement adapter

---
+++ app/src/main/java/com/example/rocketman/rocket/list/Adapter.kt
@@ -0,0 +1,35 @@
+package com.example.rocketman.rocket.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.rocketman.databinding.ItemRocketBinding
+import com.example.rocketman.rocket.Rocket
+
+class Adapter: ListAdapter<Rocket, Holder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(
+            ItemRocketBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+private class DiffCallback: DiffUtil.ItemCallback<Rocket>() {
+    override fun areItemsTheSame(oldItem: Rocket, newItem: Rocket): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Rocket, newItem: Rocket): Boolean {
+        return oldItem == newItem
+    }
+}
