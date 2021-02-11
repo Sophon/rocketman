9591867ac7626ecd78975c068273e49b083717dd
Le Duy Anh <lenduya@gmail.com>
2021-02-11 23:08:04 +0100

    Rename: RocketsData -> RocketList

--- app/src/main/java/com/example/rocketman/rocket/RocketList.kt
+++ app/src/main/java/com/example/rocketman/rocket/RocketList.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.rocketman.databinding.FragmentRocketsDataBinding
 
-class RocketsDataFragment: Fragment() {
+class RocketList: Fragment() {
 
     private lateinit var binding: FragmentRocketsDataBinding
 
