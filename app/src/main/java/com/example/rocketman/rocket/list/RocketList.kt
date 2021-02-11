2642c2d9bd56572a92945bad7b713935118f289c
Le Duy Anh <lenduya@gmail.com>
2021-02-11 23:47:37 +0100

    Display RV

--- app/src/main/java/com/example/rocketman/rocket/list/RocketList.kt
+++ app/src/main/java/com/example/rocketman/rocket/list/RocketList.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import com.example.rocketman.databinding.FragmentRocketsDataBinding
 import com.example.rocketman.rocket.Repo
+import com.example.rocketman.rocket.Rocket
 import timber.log.Timber
 
 class RocketList: Fragment() {
@@ -30,6 +33,8 @@ class RocketList: Fragment() {
     ): View {
         binding = FragmentRocketsDataBinding.inflate(inflater)
 
+        setupRecyclerView()
+
         return binding.root
     }
 
@@ -39,9 +44,22 @@ class RocketList: Fragment() {
         setupObservers()
     }
 
+    //region RecyclerView
+    private fun setupRecyclerView() {
+        binding.rvRockets.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = Adapter()
+        }
+    }
+
+    private fun updateList(rocketList: List<Rocket>) {
+        (binding.rvRockets.adapter as Adapter).submitList(rocketList)
+    }
+    //endregion
+
     private fun setupObservers() {
         vm.rockets.observe(viewLifecycleOwner) {
-            //TODO: setupUI
+            updateList(it)
         }
     }
 }
