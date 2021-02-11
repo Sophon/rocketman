0fc6286a9673e4f6ef9829880d211c2cb8d26132
Le Duy Anh <lenduya@gmail.com>
2021-02-11 23:22:28 +0100

    Connect VM

--- app/src/main/java/com/example/rocketman/rocket/RocketList.kt
+++ app/src/main/java/com/example/rocketman/rocket/RocketList.kt
@@ -5,11 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.rocketman.databinding.FragmentRocketsDataBinding
+import timber.log.Timber
 
 class RocketList: Fragment() {
 
     private lateinit var binding: FragmentRocketsDataBinding
+    private val vm by lazy {
+        ViewModelProvider(this).get(RocketListVM::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Repo.init(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,5 +36,12 @@ class RocketList: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        vm.rockets.observe(viewLifecycleOwner) {
+            Timber.d("bullshit: ${it.size} rockets fetched")
+        }
     }
 }
