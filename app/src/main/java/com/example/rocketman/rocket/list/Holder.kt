a6cc377e552499b44682727d6cf5c4943115f5de
Le Duy Anh <lenduya@gmail.com>
2021-02-12 00:20:35 +0100

    Display first image of the rocket

--- app/src/main/java/com/example/rocketman/rocket/list/Holder.kt
+++ app/src/main/java/com/example/rocketman/rocket/list/Holder.kt
@@ -1,21 +1,28 @@
 package com.example.rocketman.rocket.list
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.rocketman.R
 import com.example.rocketman.databinding.ItemRocketBinding
 import com.example.rocketman.rocket.Rocket
+import com.squareup.picasso.Picasso
 
 class Holder(
     private val binding: ItemRocketBinding
 ): RecyclerView.ViewHolder(binding.root) {
 
-    private lateinit var rocket: Rocket
-
     fun bind(rocket: Rocket) {
-        this.rocket = rocket
-
         binding.apply {
             txtName.text = rocket.name
             txtFirstFlight.text = rocket.firstFlight
+
+            if(rocket.flickrImages.isNotEmpty()) {
+                Picasso.get()
+                    .load(rocket.flickrImages[0])
+                    .placeholder(R.drawable.ic_rocket)
+                    .fit()
+                    .centerCrop()
+                    .into(imgRocket)
+            }
         }
     }
 }
