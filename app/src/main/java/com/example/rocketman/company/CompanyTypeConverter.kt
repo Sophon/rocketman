d312caccadbeaf1a44fb6fc9846b48522cb6daa4
Le Duy Anh <lenduya@gmail.com>
2021-02-15 03:13:29 +0100

    Make converters nullable

--- app/src/main/java/com/example/rocketman/company/CompanyTypeConverter.kt
+++ app/src/main/java/com/example/rocketman/company/CompanyTypeConverter.kt
@@ -5,36 +5,42 @@ import androidx.room.TypeConverter
 class CompanyTypeConverter {
 
     @TypeConverter
-    fun fromHqToString(hq: Headquarters): String {
-        return hq.toString()
+    fun fromHqToString(hq: Headquarters?): String? {
+        return hq?.toString()
     }
 
     @TypeConverter
-    fun fromStringToHq(string: String): Headquarters {
-        val array = string.split("\n")
+    fun fromStringToHq(str: String?): Headquarters? {
+        str?.let {
+            val array = str.split("\n")
 
-        return Headquarters(
-            address = array[0],
-            city =  array[1],
-            state = array[2]
-        )
+            return Headquarters(
+                address = array[0],
+                city =  array[1],
+                state = array[2]
+            )
+        }
+        return null
     }
 
     @TypeConverter
-    fun fromLinksToString(links: Links): String {
-        return links.toString()
+    fun fromLinksToString(links: Links?): String? {
+        return links?.toString()
     }
 
     @TypeConverter
-    fun fromStringToLinks(string: String): Links {
-        val array = string.split(";")
+    fun fromStringToLinks(str: String?): Links? {
+        str?.let {
+            val array = str.split(";")
 
-        return Links(
-            elonTwitter = array[0],
-            flickr = array[1],
-            twitter = array[2],
-            website = array[3]
-        )
+            return Links(
+                elonTwitter = array[0],
+                flickr = array[1],
+                twitter = array[2],
+                website = array[3]
+            )
+        }
+        return null
     }
 
 }
