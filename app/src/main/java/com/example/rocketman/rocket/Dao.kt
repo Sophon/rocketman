97c20cd3aefcb22d9ac644c8490396092265397e
Le Duy Anh <lenduya@gmail.com>
2021-02-15 02:25:39 +0100

    Implement Dao for Rocket

---
+++ app/src/main/java/com/example/rocketman/rocket/Dao.kt
@@ -0,0 +1,15 @@
+package com.example.rocketman.rocket
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface Dao {
+
+    @Query("SELECT * FROM rocket")
+    fun getRockets(): Flow<List<Rocket>>
+
+    @Query("SELECT * FROM rocket WHERE id = :id")
+    fun getRocket(id: String): Flow<Rocket>
+}
