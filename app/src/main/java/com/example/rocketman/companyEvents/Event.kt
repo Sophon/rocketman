4f194dad462800e3de7531a11beaecadeb6cd1f1
Le Duy Anh <lenduya@gmail.com>
2021-02-15 18:26:53 +0100

    Create Event data class

---
+++ app/src/main/java/com/example/rocketman/companyEvents/Event.kt
@@ -0,0 +1,21 @@
+package com.example.rocketman.companyEvents
+
+import com.google.gson.annotations.SerializedName
+
+data class Event(
+    @SerializedName("details")
+    val details: String = "",
+    @SerializedName("event_date_unix")
+    val eventDateUnix: Int = 0,
+    @SerializedName("event_date_utc")
+    val eventDateUtc: String = "",
+    @SerializedName("links")
+    val links: Links = Links(),
+    @SerializedName("title")
+    val title: String = ""
+)
+
+data class Links(
+    @SerializedName("article")
+    val article: String = ""
+)
