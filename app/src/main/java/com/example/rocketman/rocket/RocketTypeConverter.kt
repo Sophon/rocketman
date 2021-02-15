9c92960ca17559d0aa1989a569efbfd102fcbac8
Le Duy Anh <lenduya@gmail.com>
2021-02-15 01:58:31 +0100

    Finish rest of converters

--- app/src/main/java/com/example/rocketman/rocket/RocketTypeConverter.kt
+++ app/src/main/java/com/example/rocketman/rocket/RocketTypeConverter.kt
@@ -56,6 +56,166 @@ class RocketTypeConverter {
     }
 
     @TypeConverter
+    fun fromFirstStageToString(firstStage: FirstStage): String {
+        firstStage.apply {
+            return "$burnTimeSec" +
+                    ";" + "$engines" +
+                    ";" + "$fuelAmountTons" +
+                    ";" + (if(reusable) "1" else "0") +
+                    ";" +  fromThrustSeaLevelToString(thrustSeaLevel) +
+                    ";" + fromThrustVacuumToString(thrustVacuum)
+        }
+    }
+
+    @TypeConverter
+    fun fromStringToFirstStage(str: String): FirstStage {
+        str.split(";").also {
+            return FirstStage(
+                burnTimeSec = it[0].toInt(),
+                engines = it[1].toInt(),
+                fuelAmountTons = it[2].toDouble(),
+                reusable = it[3] == "1",
+                thrustSeaLevel = fromStringToThrustSeaLevel(it[4]),
+                thrustVacuum = fromStringToThrustVacuum(it[5])
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromHeightToString(height: Height): String {
+        return "${height.feet};${height.meters}"
+    }
+
+    @TypeConverter
+    fun fromStringToHeight(str: String): Height {
+        str.split(";").also {
+            return Height(
+                feet = it[0].toDouble(),
+                meters = it[1].toDouble()
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromLadingLegsToString(landingLegs: LandingLegs): String {
+        return "${landingLegs.material};${landingLegs.number}"
+    }
+
+    @TypeConverter
+    fun fromStringToLandingLegs(str: String): LandingLegs {
+        str.split(";").also {
+            return LandingLegs(
+                material = it[0],
+                number = it[1].toInt()
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromMassToString(mass: Mass): String {
+        return "${mass.kg};${mass.lb}"
+    }
+
+    @TypeConverter
+    fun fromStringToMass(str: String): Mass {
+        str.split(";").also {
+            return Mass(
+                kg = it[0].toInt(),
+                lb = it[1].toInt()
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromPayLoadWeightToString(payloadWeight: PayloadWeight): String {
+        return "${payloadWeight.id};${payloadWeight.kg};${payloadWeight.lb};${payloadWeight.name}"
+    }
+
+    @TypeConverter
+    fun fromStringToPayloadWeight(str: String): PayloadWeight {
+        str.split(";").also {
+            return PayloadWeight(
+                id = it[0],
+                kg = it[1].toInt(),
+                lb = it[2].toInt(),
+                name = it[3]
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromSecondStageToString(secondStage: SecondStage): String {
+        secondStage.apply {
+            return "$burnTimeSec" +
+                    ";" + "$engines" +
+                    ";" + "$fuelAmountTons" +
+                    ";" + fromPayloadsToString(payloads) +
+                    ";" + (if(reusable) "1" else "0") +
+                    ";" + fromThrustToString(thrust)
+        }
+    }
+
+    @TypeConverter
+    fun fromStringToSecondStage(str: String): SecondStage {
+        str.split(";").also {
+            return SecondStage(
+                burnTimeSec = it[0].toInt(),
+                engines = it[1].toInt(),
+                fuelAmountTons = it[2].toDouble(),
+                payloads = fromStringToPayloads(it[3]),
+                reusable = it[4] == "1",
+                thrust = fromStringToThrust(it[5])
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromThrustToString(thrust: Thrust): String {
+        return "${thrust.kN};${thrust.lbf}"
+    }
+
+    @TypeConverter
+    fun fromStringToThrust(str: String): Thrust {
+        str.split(";").also {
+            return Thrust(
+                kN = it[0].toInt(),
+                lbf = it[1].toInt()
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromPayloadsToString(payloads: Payloads): String {
+        return fromCompositeFairingToString(payloads.compositeFairing) + "${payloads.option1}"
+    }
+
+    @TypeConverter
+    fun fromStringToPayloads(str: String): Payloads {
+        str.split(";").also {
+            return Payloads(
+                compositeFairing = fromStringToCompositeFairing(it[0]),
+                option1 = it[1]
+            )
+        }
+    }
+
+    @TypeConverter
+    fun fromCompositeFairingToString(compositeFairing: CompositeFairing): String {
+        return fromDiameterToString(compositeFairing.diameter) +
+                fromHeightToString(compositeFairing.height)
+    }
+
+    @TypeConverter
+    fun fromStringToCompositeFairing(str: String): CompositeFairing {
+        str.split(";").also {
+            return CompositeFairing(
+                diameter = fromStringToDiameter(it[0]),
+                height =  fromStringToHeight(it[1])
+            )
+        }
+    }
+
+    @TypeConverter
     fun fromIspToString(isp: Isp): String {
         return "${isp.seaLevel};${isp.vacuum}"
     }
