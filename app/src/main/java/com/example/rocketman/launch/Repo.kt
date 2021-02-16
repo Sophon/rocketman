793b1aa214660ebf9d08a232f6de331613348e49
Le Duy Anh <lenduya@gmail.com>
2021-02-16 21:21:00 +0100

    Create launch repo

---
+++ app/src/main/java/com/example/rocketman/launch/Repo.kt
@@ -0,0 +1,36 @@
+package com.example.rocketman.launch
+
+import android.content.Context
+import com.example.rocketman.common.BASE_URL_SPACEX
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.IllegalStateException
+
+private const val ERROR_MSG_NO_INSTANCE = "Launch repository must be initialized!"
+
+class Repo private constructor(context: Context) {
+
+    private val api by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL_SPACEX)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(Api::class.java)
+    }
+
+    suspend fun getRemoteRockets() = api.getLaunches()
+
+    companion object {
+        private var INSTANCE: Repo? = null
+
+        fun init(context: Context) {
+            if(INSTANCE == null) {
+                INSTANCE = Repo(context)
+            }
+        }
+
+        fun get(): Repo {
+            return INSTANCE ?: throw IllegalStateException(ERROR_MSG_NO_INSTANCE)
+        }
+    }
+}
