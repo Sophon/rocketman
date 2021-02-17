2b4eb44678d5d352e49b05d788d7050bda55f714
Le Duy Anh <lenduya@gmail.com>
2021-02-17 01:58:28 +0100

    Display launches

--- app/src/main/java/com/example/rocketman/launch/list/LaunchListFragment.kt
+++ app/src/main/java/com/example/rocketman/launch/list/LaunchListFragment.kt
@@ -5,11 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rocketman.databinding.FragmentLaunchListBinding
+import com.example.rocketman.launch.Launch
+import com.example.rocketman.launch.Repo
 
 class LaunchListFragment: Fragment() {
 
     private lateinit var binding: FragmentLaunchListBinding
+    private val vm by lazy {
+        ViewModelProvider(this).get(LaunchListVM::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Repo.init(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +31,33 @@ class LaunchListFragment: Fragment() {
     ): View {
         binding = FragmentLaunchListBinding.inflate(inflater)
 
+        setupRecyclerView()
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupObservers()
+    }
+
+    //region RecyclerView
+    private fun setupRecyclerView() {
+        binding.rvLaunches.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = Adapter(requireContext())
+        }
+    }
+
+    private fun updateList(launchList: List<Launch>) {
+        (binding.rvLaunches.adapter as Adapter).submitList(launchList)
+    }
+    //endregion
+
+    private fun setupObservers() {
+        vm.launches.observe(viewLifecycleOwner) {
+            updateList(it)
+        }
     }
 }
