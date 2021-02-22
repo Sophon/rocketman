b6470789140768ce4657447d49e7985cc03f985e
Le Duy Anh <lenduya@gmail.com>
2021-02-22 20:01:21 +0100

    Fix packaging name

--- app/src/main/java/com/example/rocketman/RocketManApp.kt
+++ app/src/main/java/com/example/rocketman/RocketManApp.kt
@@ -1,11 +1,8 @@
 package com.example.rocketman
 
 import android.app.Application
-import com.example.rocketman.di.allModules
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 import timber.log.Timber
 
 class RocketManApp: Application() {
