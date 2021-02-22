16903c67a70fb081597a767824c787c12e49c17a
Le Duy Anh <lenduya@gmail.com>
2021-02-22 22:01:07 +0100

    Remove unused imports and funs

--- app/src/main/java/com/example/rocketman/features/event/Adapter.kt
+++ app/src/main/java/com/example/rocketman/features/event/Adapter.kt
@@ -1,6 +1,5 @@
 package com.example.rocketman.features.event
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
